29d74c830d14303fe9659e281e0223c6a3f90f7c
thwamster <thwamster@gmail.com>
2026-01-14 21:01:53 -0800

    01 wc Completed

--- src/wc.c
+++ src/wc.c
@@ -1,192 +1,322 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define MAX_FILES 1024
-#define MAX_LENGTH 1024
-
-#define NEWLINES 0
-#define WORDS 1
-#define CHARS 2
-#define BYTES 3
-#define MAX_LINE_LENGTH 4
-
-#define HELP_MENU \
-	"Usage: wc [OPTION]... [FILE]...\n" \
-	"  or:  wc [OPTION]... --files0-from=F\n" \
-	"Print newline, word, and byte counts for each FILE, and a total line if\n" \
-	"more than one FILE is specified.  A word is a non-zero-length sequence of\n" \
-	"characters delimited by white space.\n" \
-	"\n" \
-	"With no FILE, or when FILE is -, read standard input.\n" \
-	"\n" \
-	"The options below may be used to select which counts are printed, always in\n" \
-	"the following order: newline, word, character, byte, maximum line length.\n" \
-	"  -c, --bytes            print the byte counts\n" \
-	"  -m, --chars            print the character counts\n" \
-	"  -l, --lines            print the newline counts\n" \
-	"      --files0-from=F    read input from the files specified by\n" \
-	"                           NUL-terminated names in file F;\n" \
-	"                           If F is - then read names from standard input\n" \
-	"  -L, --max-line-length  print the maximum display width\n" \
-	"  -w, --words            print the word counts\n" \
-	"      --help     display this help and exit\n" \
-	"      --version  output version information and exit\n" /* "\n" \
-	"GNU coreutils online help: <https://www.gnu.org/software/coreutils/>\n" \
-	"Report any translation bugs to <https://translationproject.org/team/>\n" \
-	"Full documentation <https://www.gnu.org/software/coreutils/wc>\n" \
-	"or available locally via: info '(coreutils) wc invocation'" */
-
-#define VERSION_MENU \
-	"wc (GNU coreutils) 9.9\n" \
-	"Copyright (C) 2025 Free Software Foundation, Inc.\n" \
-	"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n" \
-	"This is free software: you are free to change and redistribute it.\n" \
-	"There is NO WARRANTY, to the extent permitted by law.\n" /* "\n" \
-	"Written by Paul Rubin and David MacKenzie." */
-
-#define ERROR_TEXT "Try 'wc --help' for more information."
-
-void get(FILE *path, int info[][5], int n);
+#include "wc.h"
 
 int main(const int argc, char **argv) {
-	char file_paths[MAX_FILES][MAX_LENGTH];
-	int info[MAX_FILES][5];
-	int standard = 1;
-	int files = 0;
-
-	info[0][CHARS] = -1;
-	info[0][MAX_LINE_LENGTH] = -1;
+	file_data[0][CHARS] = -1;
+	file_data[0][MAX_LINE_LENGTH] = -1;
 
 	for (int i = 1; i < argc; i++) {
 		const char *arg = argv[i];
-		int value = -1;
 
-		if (strcmp(arg, "--help") == 0) {
-			printf(HELP_MENU);
+		if (parse_argument(arg) == 1) {
 			return 0;
 		}
-		if (strcmp(arg, "--version") == 0) {
-			printf(VERSION_MENU);
-			return 0;
-		}
-		if (strncmp(arg, "-", 1) == 0) {
-			if (strcmp(arg, "-c") == 0 || strcmp(arg, "--bytes") == 0) {
-				value = BYTES;
-			}
-			else if (strcmp(arg, "-m") == 0 || strcmp(arg, "--chars") == 0) {
-				value = CHARS;
-			}
-			else if (strcmp(arg, "-l") == 0 || strcmp(arg, "--lines") == 0) {
-				value = NEWLINES;
-			}
-			else if (strcmp(arg, "-L") == 0 || strcmp(arg, "--max-line-length") == 0) {
-				value = MAX_LINE_LENGTH;
-			}
-			else if (strcmp(arg, "-w") == 0 || strcmp(arg, "--words") == 0) {
-				value = WORDS;
-			}
-			else if (strncmp(arg, "--files0-from", 13) == 0) {
-				// TODO: implement
-				continue;
-			}
+	}
+	input();
+	output();
 
-			if (value == -1) {
-				printf("my_wc: unrecognized option '%s'\nTry 'my_wc --help' for more information.\n", arg);
-				return 0;
-			}
+	return 0;
+}
 
-			if (standard == 1) {
-				info[0][NEWLINES] = -1;
-				info[0][WORDS] = -1;
-				info[0][BYTES] = -1;
-				standard = 0;
-			}
+void input() {
+	for (int i = 1; i <= file_count; i++) {
+		const char *path = file_paths[i];
 
-			info[0][value] = 0;
+		if (strcmp(path, "-") == 0) {
+			analyze_file(stdin, i);
 			continue;
 		}
 
-		files++;
-		strcpy(file_paths[files], arg);
-	}
-
-	for (int i = 1; i <= files; i++) {
-		const char *path = file_paths[i];
-		FILE *file = fopen(path, "r");
+		FILE *file = fopen(file_paths[i], "r");
 
 		if (file == NULL) {
-			info[i][0] = -1;
+			file_data[i][0] = -1;
 			continue;
 		}
 
-		get(file, info, i);
-
+		analyze_file(file, i);
 		fclose(file);
 	}
 
-	if (files == 0) {
-		get(stdin, info, 1);
-		files = 1;
+	if (file_count == 0) {
+		analyze_file(stdin, 1);
+		file_count = 1;
+	}
+}
+
+void output() {
+	for (int j = 0; j < 5; j++) {
+		if (file_data[0][j] < 0) {
+			continue;
+		}
+
+		int width = 0;
+
+		for (int i = 0; i <= file_count; i++) {
+			const int value = file_data[i][j];
+
+			if (value > 0) {
+				const int new_width = (int) floor(log10(abs(value))) + 1;
+
+				if (width < new_width) {
+					width = new_width;
+				}
+			}
+		}
+
+		file_data[file_count + 1][j] = width;
 	}
 
-	// TODO: fix alignment
-	for (int i = 1; i <= files; i++) {
-		if (info[i][0] < 0) {
-			printf("my_wc: %s: No such file or directory\n", file_paths[i]);
+	for (int i = 1; i <= file_count; i++) {
+		const char *path = file_paths[i];
+
+		if (strlen(path) > MAX_LENGTH) {
+			throw_error(path, TOO_LONG);
+			continue;
+		}
+
+		if (file_data[i][0] < 0) {
+			throw_error(path, NO_SUCH);
 			continue;
 		}
 
 		for (int j = 0; j < 5; j++) {
-			if (info[0][j] >= 0) {
-				info[0][j] += info[i][j];
-				printf(" %d", info[i][j]);
+			if (file_data[0][j] >= 0) {
+				printf(" %*d", file_data[file_count + 1][j], file_data[i][j]);
 			}
 		}
 
-		printf(" %s\n", file_paths[i]);
+		printf(" %s\n", path);
 	}
 
-	if (files > 1) {
-		for (int i = 0; i < 5; i++) {
-			if (info[0][i] >= 0) {
-				printf(" %d", info[0][i]);
+	if (file_count > 1) {
+		for (int j = 0; j < 5; j++) {
+			if (file_data[0][j] >= 0) {
+				printf(" %*d", file_data[file_count + 1][j], file_data[0][j]);
 			}
 		}
 		printf(" total\n");
+	}
+}
+
+int parse_argument(const char *s) {
+	if (strcmp(s, "-") != 0) {
+		if (strncmp(s, "--", 2) == 0) {
+			return parse_option_long(s + 2);
+		}
+		if (strncmp(s, "-", 1) == 0) {
+			return parse_option_short(s + 1);
+		}
+	}
+
+	return parse_file(s);
+}
+
+int parse_file(const char *s) {
+	if (enable_files0 == 1) {
+		return throw_error(s, EXTRA_OPERAND);
+	}
+
+	enable_files0 = 0;
+	file_count += 1;
+	strcpy(file_paths[file_count], s);
+	return 0;
+}
+
+int parse_files0(FILE *f) {
+	char c;
+	int len = 0;
+	file_count = 1;
+
+	while ((c = (char) getc(f)) != EOF) {
+		if (c == '\0') {
+			file_count += 1;
+			len = 0;
+
+			FILE *now = fopen(file_paths[file_count], "r");
+			if (now == NULL) {
+				return throw_error(file_paths[file_count], CANNOT_OPEN);
+			}
+			fclose(now);
+		}
+
+		file_paths[file_count][len] = c;
+		file_paths[file_count][len + 1] = '\0';
+		len++;
+	}
+
+	FILE *now = fopen(file_paths[file_count], "r");
+	if (now == NULL) {
+		return throw_error(file_paths[file_count], CANNOT_OPEN);
+	}
+	fclose(now);
+
+	return 0;
+}
+
+int parse_option_long(const char *s) {
+	if (strcmp(s, "help") == 0) {
+		printf(HELP_MENU);
+		return 1;
+	}
+	if (strcmp(s, "version") == 0) {
+		printf(VERSION_MENU);
+		return 1;
+	}
+	if (strncmp(s, "files0-from=", 12) == 0) {
+		if (enable_files0 == 0) {
+			return throw_error(s, EXTRA_OPERAND);
+		}
+
+		enable_files0 = 1;
+
+		FILE *file = (strcmp(s, "-") == 0) ? file = stdin : fopen(s + 12, "r");
+		if (file == NULL) {
+			return throw_error(s, NO_SUCH);
+		}
+		return parse_files0(file);
+	}
+	if (strcmp(s, "bytes") == 0) {
+		enable_option(BYTES);
+	}
+	else if (strcmp(s, "chars") == 0) {
+		enable_option(CHARS);
+	}
+	else if (strcmp(s, "lines") == 0) {
+		enable_option(NEWLINES);
+	}
+	else if (strcmp(s, "max-line-length") == 0) {
+		enable_option(MAX_LINE_LENGTH);
+	}
+	else if (strcmp(s, "words") == 0) {
+		enable_option(WORDS);
+	}
+	else {
+		return throw_error(s, UNRECOGNIZED_OPTION);
 	}
 
 	return 0;
 }
 
-// TODO: fix output
-void get(FILE *path, int info[][5], const int n) {
+int parse_option_short(const char *s) {
+
+	while (strlen(s) > 0) {
+		if (strncmp(s, "c", 1) == 0) {
+			enable_option(BYTES);
+		}
+		else if (strncmp(s, "m", 1) == 0) {
+			enable_option(CHARS);
+		}
+		else if (strncmp(s, "l", 1) == 0) {
+			enable_option(NEWLINES);
+		}
+		else if (strncmp(s, "L", 1) == 0) {
+			enable_option(MAX_LINE_LENGTH);
+		}
+		else if (strncmp(s, "w", 1) == 0) {
+			enable_option(WORDS);
+		}
+		else {
+			return throw_error(s, INVALID_OPTION);
+		}
+
+		s = s + 1;
+	}
+
+	return 0;
+}
+
+void enable_option(const int n) {
+	if (enable_standard == 1) {
+		file_data[0][NEWLINES] = -1;
+		file_data[0][WORDS] = -1;
+		file_data[0][BYTES] = -1;
+		enable_standard = 0;
+	}
+
+	file_data[0][n] = 0;
+}
+
+void analyze_file(FILE *f, const int n) {
 	char c;
 	int line_length = 0;
 	int word_length = 0;
-	while ((c = (char) getc(path)) != EOF) {
-		info[n][CHARS] += 1;
-		info[n][BYTES] += 1;
 
-		if (!isspace(c)) {
-			line_length += 1;
-			word_length += 1;
+	while ((c = (char) getc(f)) != EOF) {
+		file_data[n][CHARS] += 1;
+		file_data[n][BYTES] += 1;
+
+		if (c == '\t') {
+			line_length += 8 - (line_length % 8);
 		}
-
-		if (c == '\n') {
-			if (info[n][MAX_LINE_LENGTH] < line_length) {
-				info[n][MAX_LINE_LENGTH] = line_length;
+		else if (c == '\n') {
+			line_length = line_length > 0 ? line_length - 1 : 0;
+			if (file_data[n][MAX_LINE_LENGTH] < line_length) {
+				file_data[n][MAX_LINE_LENGTH] = line_length;
 			}
-
-			info[n][NEWLINES] += 1;
+			file_data[n][NEWLINES] += 1;
 			line_length = 0;
 		}
-
-		if (word_length > 0) {
-			info[n][WORDS] += 1;
+		else {
+			line_length += 1;
 		}
 
-		word_length = 0;
+		if (!isspace(c)) {
+			word_length += 1;
+		}
+		else {
+			if (word_length > 0) {
+				file_data[n][WORDS] += 1;
+				word_length = 0;
+			}
+		}
 	}
+
+	if (word_length > 0) {
+		file_data[n][WORDS] += 1;
+	}
+
+	line_length = line_length > 0 ? line_length - 1 : 0;
+	if (file_data[n][MAX_LINE_LENGTH] < line_length) {
+		file_data[n][MAX_LINE_LENGTH] = line_length;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (file_data[0][i] >= 0) {
+			file_data[0][i] += file_data[n][i];
+		}
+	}
+
+	if (file_data[0][MAX_LINE_LENGTH] >= 0 && file_data[0][MAX_LINE_LENGTH] < file_data[n][MAX_LINE_LENGTH]) {
+		file_data[0][MAX_LINE_LENGTH] = file_data[n][MAX_LINE_LENGTH];
+	}
+}
+
+int throw_error(const char *s, const int n) {
+	switch (n) {
+		case TOO_LONG: printf("my_wc: %s: File name too long\n", s); break;
+		case NO_SUCH: printf("my_wc: %s: No such file or directory\n", s); break;
+		case EXTRA_OPERAND:
+			{
+				printf("wc: extra operand '%s'\nfile operands cannot be combined with --files0-from\nTry 'wc --help' for more information.\n", s);
+				break;
+			}
+		case UNRECOGNIZED_OPTION: printf("my_wc: unrecognized option '--%s'\nTry 'my_wc --help' for more information.\n", s); break;
+		case INVALID_OPTION: printf("my_wc: invalid option -- '%c'\nTry 'my_wc --help' for more information.\n", *s); break;
+		case CANNOT_OPEN:
+			{
+				printf("wc: cannot open '");
+				for (int i = 0; i < strlen(s); i++) {
+					if (isprint(s[i])) {
+						printf("%c", s[i]);
+					}
+					else {
+						printf("'\\x%02x'", s[i]);
+					}
+				}
+				printf("' for reading: No such file or directory");
+				break;
+			}
+		default:;
+	}
+	return 1;
 }
---
+++ src/wc.h
@@ -0,0 +1,72 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILES 1024
+#define MAX_LENGTH 1024
+
+#define NEWLINES 0
+#define WORDS 1
+#define CHARS 2
+#define BYTES 3
+#define MAX_LINE_LENGTH 4
+
+#define TOO_LONG 0
+#define NO_SUCH 1
+#define EXTRA_OPERAND 2
+#define UNRECOGNIZED_OPTION 3
+#define INVALID_OPTION 4
+#define CANNOT_OPEN 5
+
+#define HELP_MENU \
+	"Usage: wc [OPTION]... [FILE]...\n" \
+	"  or:  wc [OPTION]... --files0-from=F\n" \
+	"Print newline, word, and byte counts for each FILE, and a total line if\n" \
+	"more than one FILE is specified.  A word is a non-zero-length sequence of\n" \
+	"characters delimited by white space.\n" \
+	"\n" \
+	"With no FILE, or when FILE is -, read standard input.\n" \
+	"\n" \
+	"The options below may be used to select which counts are printed, always in\n" \
+	"the following order: newline, word, character, byte, maximum line length.\n" \
+	"  -c, --bytes            print the byte counts\n" \
+	"  -m, --chars            print the character counts\n" \
+	"  -l, --lines            print the newline counts\n" \
+	"      --files0-from=F    read input from the files specified by\n" \
+	"                           NUL-terminated names in file F;\n" \
+	"                           If F is - then read names from standard input\n" \
+	"  -L, --max-line-length  print the maximum display width\n" \
+	"  -w, --words            print the word counts\n" \
+	"      --help     display this help and exit\n" \
+	"      --version  output version information and exit\n" /* "\n" \
+	"GNU coreutils online help: <https://www.gnu.org/software/coreutils/>\n" \
+	"Report any translation bugs to <https://translationproject.org/team/>\n" \
+	"Full documentation <https://www.gnu.org/software/coreutils/wc>\n" \
+	"or available locally via: info '(coreutils) wc invocation'" */
+
+#define VERSION_MENU \
+	"wc (GNU coreutils) 9.9\n" \
+	"Copyright (C) 2025 Free Software Foundation, Inc.\n" \
+	"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n" \
+	"This is free software: you are free to change and redistribute it.\n" \
+	"There is NO WARRANTY, to the extent permitted by law.\n" /* "\n" \
+	"Written by Paul Rubin and David MacKenzie." */
+
+char file_paths[MAX_FILES][MAX_LENGTH];
+int file_data[MAX_FILES][5];
+int file_count = 0;
+int enable_standard = 1;
+int enable_files0 = -1;
+
+void input();
+void output();
+int parse_file(const char *s);
+int parse_files0(FILE *f);
+int parse_argument(const char *s);
+int parse_option_long(const char *s);
+int parse_option_short(const char *s);
+void enable_option(int n);
+void analyze_file(FILE *f, int n);
+int throw_error(const char *s, int n);
