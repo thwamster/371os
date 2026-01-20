6802529022bc6e198438d9daddaa953f896e3951
thwamster <thwamster@gmail.com>
2026-01-19 17:50:30 -0800

    12 split_at Updated

--- rs/12/src/main.rs
+++ rs/12/src/main.rs
@@ -2,8 +2,8 @@ use split_at::*;
 
 fn main() {
     let mut n: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    println!("{:?}", &n);
+    println!("Original: {:?}", &n);
 
     let (a, b) = split_at_mut(&mut n, 5_usize);
-    println!("{:?}\n{:?}", &a, &b);
+    println!("Split: {:?}, {:?}", &a, &b);
 }
